aa217bca57e36611113778873294a35550b8e56d
Seth David Gyimah <esec26@gmail.com>
2022-09-11 15:26:28 +0000

    active record get lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,26 @@
 class ApplicationController < Sinatra::Base
 
+  #setting content header type:
+  set :default_content_type, 'application/json'
+
   # add routes
 
+  get "/bakeries" do 
+    Bakery.all.to_json
+  end
+
+  get "/bakeries/:id" do 
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get "/baked_goods/by_price" do 
+    baked_goods = BakedGood.all
+    baked_goods.order(price: :desc).to_json
+  end
+
+  get "/baked_goods/most_expensive" do 
+    most_expensive = BakedGood.all.order(price: :desc).first 
+    most_expensive.to_json
+  end
 end
